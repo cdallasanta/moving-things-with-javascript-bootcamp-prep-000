078906768b2130214877ddd2b6d9e20f8bd04905
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-11-26 02:42:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ let dodger = document.querySelector('#dodger');
 
 document.addEventListener('keydown', function(event){
   if(event.which === 37 && dodger.style.left !== '0px'){
-    let leftString = dodger.style.left.replace('px','');
-    let leftNum = parseInt(leftString);
-    dodger.style.left = `${leftNum-1}px`;
+    moveDodgerLeft();
   } else if (event.which === 39){
     let leftString = dodger.style.left.replace('px','');
     let leftNum = parseInt(leftString);
     dodger.style.left = `${leftNum+1}px`;
   }
 });
+
+function moveDodgerLeft(){
+  let leftString = dodger.style.left.replace('px','');
+  let leftNum = parseInt(leftString);
+  dodger.style.left = `${leftNum-1}px`;
+}
